72a234a5734c17691bc5a4bf26b6f8426e2bae40
AlstonZZY <129561525+AlstonZZY@users.noreply.github.com>
2024-01-08 00:19:03 -0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -491,7 +491,7 @@ export default {
       line-height: 22px;
       background-color: #fff;
       position: absolute;
-      bottom: 0;
+      bottom: 5;
       left: 0;
       font-size: 30px;
       // border-radius: 50%;
