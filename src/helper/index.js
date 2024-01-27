3d9046b57aaa88cc89bcfe289a21f8335776afed
AlstonZZY <129561525+AlstonZZY@users.noreply.github.com>
2024-01-26 16:46:14 -0800

    Update index.js

--- src/helper/index.js
+++ src/helper/index.js
@@ -41,7 +41,7 @@ export function conversionCategoryName(key) {
   let name = '';
   switch (key) {
     case 'firstPrize':
-      name = '一等獎：長榮航空LAX-TPE往返機票 一張';
+      name = '一等獎：長榮航空LAX至TPE豪華經濟艙機票一張';
       break;
     default:
       break;
